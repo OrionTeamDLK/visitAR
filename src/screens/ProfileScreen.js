6e65d79a4454f3393dc2048ab797f4c8d9335326
Cormac Levins <cormaclevins@outlook.ie>
2020-01-04 20:46:18 +0000

    profile Bug Fix

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,63 +1,101 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import NavigationButton from '../Components/NavigationButton';
+import { Container, Content, Header, Form, Input, Item, Button, Label } from 'native-base';
 import * as firebase from 'firebase';
 
 export default class ProfileScreen extends React.Component{
 
-  // componentDidMount(){
-  //   this.checkIfLoggedIn()
+  componentDidMount(){
+    //this.checkIfLoggedIn()
+    this.getUserDetails();
 
-  // }
+  }
 
   constructor(props){
     super(props)
   }
 
-  // checkIfLoggedIn = () => {
-  //   console.log('called');
-  //   firebase.auth().onAuthStateChanged( user => {
-  //     console.log(user);
-  //     if(!user){
-  //
-  //       this.props.navigation.navigate('Login');
-  //     }
-  //   })
-  // }
+  getUserDetails = () => {
+    const user = firebase.auth().currentUser;
+
+    console.log(user);
+    const name, email, photoUrl, uid, emailVerified;
+
+    if (user != null) {
+      name = user.displayName;
+      email = user.email;
+      photoUrl = user.photoURL;
+      emailVerified = user.emailVerified;
+      uid = user.uid;
+    }
+  }
+
+  logOut = () => {
+    firebase.auth().signOut().then( () => {
+      console.log('Signed Out');
+      this.props.navigation.navigate('Index');
+    }, function(error) {
+      console.error('Sign Out Error', error);
+    });
+
+  }
+
+  checkIfLoggedIn = () => {
+    console.log('called');
+    firebase.auth().onAuthStateChanged( user => {
+      console.log(user);
+      if(!user){
+        this.props.navigation.navigate('Index');
+      }
+    })
+  }
 
   render() {
     return (
 
       <Container style={styles.container} >
 
-      <Button style={ styles.Button }
-        full
-        rounded
-        info
-        onPress={()=>this.loginUser(this.state.email, this.state.password)}>
+        <Text>{name}</Text>
 
         <NavigationButton
+        style={ styles.Button }
         data-test = "ProfileScreen_button"
         title="Home"
         icon = "home"
         navName = "Index"/>
 
         <NavigationButton
+        style={ styles.Button }
         data-test = "ProfileScreen_button"
         title="Update Profile"
         icon = "edit"
         navName = "EditProfile"/>
 
 
-      </Container>
+        <Button style={ styles.Button }
+          full
+          rounded
+          success
+          onPress={() => this.logOut()}
+        >
+        <Text style={{ color: '#fff' }}>Log out</Text>
+        </Button>
 
+      </Container>
 
     );
   }
 
 }
 
-
-
-
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+  },
+  Button: {
+    marginTop: 10
+  },
+});
