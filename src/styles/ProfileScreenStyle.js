0609ea21e3522f069f566ab4358f7fb4b94fb9f7
Cormac Levins <cormaclevins@outlook.ie>
2020-03-15 13:45:04 +0000

    endTour screen style sheet added

--- src/styles/ProfileScreenStyle.js
+++ src/styles/ProfileScreenStyle.js
@@ -2,7 +2,7 @@ import {
     StyleSheet
 } from "react-native";
 
-const styles = StyleSheet.create({
+export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
@@ -27,5 +27,3 @@ const styles = StyleSheet.create({
     fontSize:20
   }
 });
-
-export { styles }
---
+++ src/styles/TourEndScreenStyle.js
@@ -0,0 +1,38 @@
+import {StyleSheet} from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center"
+  },
+  Button: {
+    marginTop: 10
+  },
+  textHeader: {
+    textAlign: "center",
+    fontSize: 30
+
+  },
+  textMain: {
+    textAlign: "center",
+    fontSize: 20
+  },
+  textList: {
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 20
+  },
+  viewBar: {
+    borderBottomColor: 'black',
+    borderBottomWidth: 3,
+    marginTop: 20,
+    marginBottom: 20
+  },
+  image: {
+    width: "80%",
+    height: 250,
+    left: "10%",
+    resizeMode: 'stretch'
+  }
+});
