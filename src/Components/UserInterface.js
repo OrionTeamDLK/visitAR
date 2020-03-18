563e90ff07722442351c4f3178b59bcb57ee4f18
Emma Loughran <emmaloughran2009@hotmail.co.uk>
2020-03-18 23:08:45 +0000

    added token icons to the screen

--- src/Components/UserInterface.js
+++ src/Components/UserInterface.js
@@ -4,6 +4,7 @@ import HelpInfoButton from "../Components/HelpInfoButton"
 import MenuButton from "../Components/MenuButton"
 import { withNavigation } from 'react-navigation';
 import PickUpTokenButton from "../Components/PickUpTokenButton";
+import tokenGame from "../screens/GoogleMapsScreen";
 
 HideStartedTourReCenterButton = (props) => {
     if (props.status == 0) {
@@ -103,18 +104,20 @@ class UserInterface extends Component {
     }
 
 
-
     render(props) {
         return (
             <>
 
+
+               
+           
                 <HelpInfoButton style={{
                     position: "absolute",
                     top: 15,
                     alignSelf: 'right'
                 }} navName="Help" />
 
-
+            
 
                 <MenuButton style={{
                     position: "absolute",
@@ -133,6 +136,23 @@ class UserInterface extends Component {
                 num_tokens = {this.props.num_tokens}
                 />*/}
 
+
+
+
+
+            <Image                       
+            style={{
+                 position: "absolute",
+                 top: 10,
+                 alignSelf: 'right',
+                 left:320,top :150,
+                 width: 50,
+                 height: 300,
+                 resizeMode: 'stretch'
+            }}            
+            source={{uri:'https://firebasestorage.googleapis.com/v0/b/orion-57b76.appspot.com/o/tokens%2F1.png?alt=media&token=e1b0139d-769f-4438-8f3a-0e5fe7386c34'}}
+            />
+    
 
                 {/* <HideStartedTourReCenterButton status={this.props.status} CallStartTour={this.props.CallStartTour} /> */}
                 <ShowEndTourButton status={this.props.status} endTour={this.props.endTour} />
