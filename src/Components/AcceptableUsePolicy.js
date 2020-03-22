f7aaaa4067ff9d42d66dc878c9bf097025b65511
Patrick McDonnell <paddyfro@gmail.com>
2020-03-22 17:26:50 +0000

    settings/acceptable use, setting up settings scxreen to display policys

---
+++ src/Components/AcceptableUsePolicy.js
@@ -0,0 +1,79 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+
+const AcceptableUsePolicy = (props) => {
+    return (
+        <View>
+            <Text>Acceptable use policy</Text>
+            <Text>These acceptable use policy ("Acceptable Use Policy", "AUP", "Policy") is an agreement between Mobile Application Developer ("Mobile Application Developer", "us", "we" or "our") and you ("User", "you" or "your"). This Policy sets forth the general guidelines and acceptable and prohibited uses of the VisitAR mobile application and any of its products or services (collectively, "Mobile Application" or "Services").
+
+                    Prohibited activities and uses
+
+                    You may not use the Services to engage in activity that is illegal under applicable law, that is harmful to others, or that would subject us to liability, including, without limitation, in connection with any of the following, each of which is prohibited under this Policy:
+
+                    - Disclosing sensitive personal information about others.
+                    - Collecting, or attempting to collect, personal information about third parties without their knowledge or consent.
+                    - Threatening harm to persons or property or otherwise harassing behavior.
+                    - Infringing the intellectual property or other proprietary rights of others.
+                    - Facilitating, aiding, or encouraging any of the above activities through our Services.
+
+                    System abuse
+
+                    Any User in violation of our Services security is subject to criminal and civil liability, as well as immediate account termination. Examples include, but are not limited to the following:
+
+                    - Use or distribution of tools designed for compromising security of the Services.
+                    - Intentionally or negligently transmitting files containing a computer virus or corrupted data.
+                    - Accessing another network without permission, including to probe or scan for vulnerabilities or breach security or authentication measures.
+                    - Unauthorized scanning or monitoring of data on any network or system without proper authorization of the owner of the system or network.
+
+                    Service resources
+
+                    You may not consume excessive amounts of the Services or use the Services in any way which results in performance issues or which interrupts the services for other Users. Prohibited activities that contribute to excessive use, include without limitation:
+
+                    - Deliberate attempts to overload the Services and broadcast attacks (i.e. denial of service attacks).
+                    - Engaging in any other activities that degrade the usability and performance of our Services.
+
+                    Security
+
+                    You take full responsibility for maintaining reasonable security precautions for your account. You are responsible for protecting and updating any login account provided to you for our Services. You must protect the confidentiality of your login details, and you should change your password periodically.
+
+                    Enforcement
+
+                    We reserve our right to be the sole arbiter in determining the seriousness of each infringement and to immediately take corrective actions, including but not limited to:
+
+                    - Disabling or removing any content which is prohibited by this Policy, including to prevent harm to others or to us or our Services, as determined by us in our sole discretion.
+                    - Reporting violations to law enforcement as determined by us in our sole discretion.
+                    - A failure to respond to an email from our abuse team within 2 days, or as otherwise specified in the communication to you, may result in the suspension or termination of your Services.
+
+                    Suspended and terminated User accounts due to violations will not be re-activated.
+
+                    Nothing contained in this Policy shall be construed to limit our actions or remedies in any way with respect to any of the prohibited activities. In addition, we reserve at all times all rights and remedies available to us with respect to such activities at law or in equity.
+
+                    Reporting violations
+
+                    If you have discovered and would like to report a violation of this Policy, please contact us immediately. We will investigate the situation and provide you with full assistance.
+
+                    Changes and amendments
+
+                    We reserve the right to modify this Policy or its terms relating to the Mobile Application or Services at any time, effective upon posting of an updated version of this Policy in the Mobile Application. When we do, we will send you an email to notify you. Continued use of the Mobile Application after any such changes shall constitute your consent to such changes. Policy was created with https://www.WebsitePolicies.com
+
+                    Acceptance of this policy
+
+                    You acknowledge that you have read this Policy and agree to all its terms and conditions. By using the Mobile Application or its Services you agree to be bound by this Policy. If you do not agree to abide by the terms of this Policy, you are not authorized to use or access the Mobile Application and its Services.
+
+                    Contacting us
+
+                    If you would like to contact us to understand more about this Policy or wish to contact us concerning any matter relating to it, you may send an email to orionteamdlk@gmail.com
+
+                    This document was last updated on March 21, 2020</Text>
+        </View>
+    );
+};
+
+export default AcceptableUsePolicy;
+
+
+const styles = StyleSheet.create({
+
+});
--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,22 +1,86 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, { Component } from 'react';
+import {View, Text, StyleSheet, Modal, TouchableHighlight} from 'react-native';
 import NavigationButton from '../Components/NavigationButton';
+import AcceptableUsePolicy from '../Components/AcceptableUsePolicy';
+import { withNavigation } from 'react-navigation';
 
 const SettingsScreen =()=>{
     return (
         <View data-test = "SettingsScreen_view">
             <Text style={{fontWeight:"bold", fontSize: 30}}data-test = "SettingsScreen_text">Settings screen</Text>
-            <Text style={styles.textStyle} data-test = "SettingsScreen_text">-Permissions</Text>
-            <Text style={styles.textStyle} data-test = "SettingsScreen_text">-Terms and conditions</Text>
-            <Text style={styles.textStyle}  data-test = "SettingsScreen_text">-Logout</Text>
+            <Text  data-test = "SettingsScreen_text">-Permissions</Text>
+            <Text  data-test = "SettingsScreen_text">-Terms and conditions</Text>
+            <Text  data-test = "SettingsScreen_text">-Logout</Text>
         </View>
     );
 };
+
+
+
+export default SettingsScreen;
+
+{/*
+class SettingsScreen extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+          modalVisible:false,
+        }
+    }
+    setModalVisible = () => {
+      //console.log("modal called and it is set to: " + this.state.modalVisible);
+      this.setState({
+        modalVisible:!this.state.modalVisible
+      })
+    }
+
+    render(props) {
+        return (
+          <View data-test = "SettingsScreen_view">
+              <Text style={{fontWeight:"bold", fontSize: 30}}data-test = "SettingsScreen_text">Settings screen</Text>
+              <Text style={styles.textStyle} data-test = "SettingsScreen_text">-Permissions</Text>
+              <Text style={styles.textStyle} data-test = "SettingsScreen_text">-Terms and conditions</Text>
+              <Text style={styles.textStyle}  data-test = "SettingsScreen_text">-Logout</Text>
+                <TouchableHighlight
+                  onPress={() => {
+                    this.setModalVisible()});
+                  }}>
+                  <Text
+                    style={{fontSize:50, fontWeight:'bold',}}
+                    >Close</Text>
+                </TouchableHighlight>
+          </View>
+                <Modal
+                animationType="fade"
+                transparent={true}
+                visible={this.state.modalVisible}
+                onRequestClose={() => {
+                  console.log('Modal has been closed.');
+                }}>
+                <View style={styles.modalOuter}>
+                    <View style={styles.modalInner}>
+                            <Text>Number of tokens you have collected so far, collect more at each landmark to fill up all the slots</Text>
+                            <TouchableHighlight
+                              onPress={() => {
+                                this.setModalVisible();
+                              }}>
+                              <Text
+                                style={{fontSize:22, fontWeight:'bold'}}
+                                >Close</Text>
+                            </TouchableHighlight>
+                    </View>
+                  </View>
+              </Modal>
+
+        )
+    }
+}
+export default withNavigation(UserInterface)
 
 const styles = StyleSheet.create({
     textStyle: {
         fontSize:25
     }
 });
-
-export default SettingsScreen;
+*/}
