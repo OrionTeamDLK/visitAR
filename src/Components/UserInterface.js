12bfbd35c09be5e8592918c4aa731eecd2759736
Patrick McDonnell <paddyfro@gmail.com>
2020-03-23 12:43:32 +0000

    changed style on token info modal

--- src/Components/UserInterface.js
+++ src/Components/UserInterface.js
@@ -285,7 +285,8 @@ const styles = StyleSheet.create({
     width: Dimensions.get('window').width * 0.5,
     height: Dimensions.get('window').height * 0.4,
     backgroundColor: '#EBD5B3',
-    padding: 20
+    padding: 20,
+    borderRadius:15
   },
   closeButton:{
     marginTop:50,
