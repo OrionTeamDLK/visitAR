f96e47ae194d718d4067113f8b462a90140f1b6e
Cormac Levins <cormaclevins@outlook.ie>
2020-03-25 12:31:51 +0000

    RwgisterStyleSheet Added

---
+++ src/styles/RegisterScreenStyle.js
@@ -0,0 +1,25 @@
+import {StyleSheet} from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center"
+  },
+  Button: {
+    marginTop: 10
+  },
+  profile_pic: {
+    width: 200,
+    height: 200,
+    borderRadius: 200 / 2,
+    overflow: "hidden",
+    borderWidth: 3,
+    borderColor: "black",
+    marginTop: 10,
+    marginBottom: 10
+  },
+  spinnerTextStyle: {
+    color: "#FFF"
+  }
+});
