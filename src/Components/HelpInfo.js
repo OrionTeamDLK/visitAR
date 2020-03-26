fe595f34440e85c52f951e0a00e3ef0b5ca34406
Patrick McDonnell <paddyfro@gmail.com>
2020-03-26 16:12:12 +0000

    added navigation button to google maps screen

--- src/Components/HelpInfo.js
+++ src/Components/HelpInfo.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet,TouchableOpacity,Dimensions} from 'react-native';
+import Icon from "react-native-vector-icons/FontAwesome";
+import { withNavigation } from 'react-navigation';
 
 const HelpInfo = (props) => {
     return(
         <View data-test = "helpinfo_view" style={styles.slideDefault}>
             <Text data-test = "helpinfo_num"style={styles.text}>{props.num}</Text>
             <Text data-test = "helpinfo_text" style={styles.text}>{props.text} </Text>
+            <TouchableOpacity
+                onPress={ ()=>props.navigation.navigate("GMaps")}
+                style={styles.button}
+                >
+                <Text style={styles.buttonText}>
+                    <Icon name="map-signs" size={30} color="white" style={styles.iconStyle}/>
+                    <Text>  </Text>
+                     Onwards to VisitAR
+                </Text>
+            </TouchableOpacity>
         </View>
     );
 };
@@ -31,7 +43,29 @@ const styles = StyleSheet.create({
 		fontSize: 40,
         fontWeight: "bold",
         textAlign:"center",
-    }
+    },
+    button: {
+      borderWidth: 0.1,
+      borderColor: '#e4d9c0',
+      borderRadius: 20,
+      height: Dimensions.get('window').height*.08,
+      width: Dimensions.get('window').width * .7,
+      backgroundColor: '#4c6294',
+      borderBottomWidth: 3,
+      borderColor: "black",
+      alignItems:'center',
+      justifyContent: 'center',
+      marginTop:5
+      },
+      buttonText:{
+        color:"white",
+        fontSize:22
+      },
+      iconStyle: {
+        width: 22,
+        height: 22,
+
+      },
 });
 
-export default HelpInfo;
+export default withNavigation(HelpInfo);
