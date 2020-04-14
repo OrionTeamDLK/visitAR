8e27f74aee981d68c059d472cd282f12642a3a08
paddyfro <paddyfro@gmail.com>
2020-04-14 13:06:24 +0100

    accessability icon displaying on carlingford castle

--- src/Components/InfoPopUp.js
+++ src/Components/InfoPopUp.js
@@ -1,15 +1,27 @@
 import React from "react";
-import { View, Text, StyleSheet, Image, FlatList } from "react-native";
+import { View, Text, StyleSheet, Image, FlatList} from "react-native";
 
 
 const InfoPopUp = (props) => {
     const desc = props.description;
     return (
-        <View style={styles.boxStyle}>
+      <View style={styles.boxStyle}>
+      {props.title ==="Carlingford Castle" ?
+      <Text style={styles.titleStyle}>{props.title}
+        <Image
+          style={styles.tinyLogo}
+          source={require('../../assets/acessNo.png')}
+          resizeMode="stretch"
+        />
+      </Text>
+        :
         <Text style={styles.titleStyle}>{props.title}</Text>
-        <Text style={styles.contentStyle}>{desc.substring(0, 60) + "..."}</Text>
-        <Text style={styles.linkStyle}>Click For More Information...</Text>
-        </View>
+      }
+      <Text style={styles.contentStyle}>{desc.substring(0, 60) + "..."}</Text>
+      <Text style={styles.linkStyle}>Click For More Information...</Text>
+
+
+      </View>
     );
 };
 
@@ -19,7 +31,7 @@ export default InfoPopUp;
 const styles = StyleSheet.create({
   boxStyle: {
     width:350,
-    height:120
+    height:150
   },
   titleStyle:{
     fontSize: 25,
@@ -34,4 +46,12 @@ const styles = StyleSheet.create({
      textAlign:"center",
      textDecorationLine:'underline'
   },
+  tinyLogo: {
+  width: 64,
+  height: 64,
+  padding:15
+},
+imgText:{
+  fontSize:72
+}
 });
